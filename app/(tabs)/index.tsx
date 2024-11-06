f6a65539467d2e79128341d680198181c90dffd4
heeyeon9578 <heeyeon9578@naver.com>
2024-11-06 17:34:11 +0900

    feat: 아이콘 추가

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,131 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, View, Text, ScrollView, Dimensions, ActivityIndicator } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import * as Location from 'expo-location';
+import React, { useEffect, useState } from 'react';
+import { Fontisto } from '@expo/vector-icons';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
+const API_KEY = '1403d5fcf20669d858eb5530c1f9199c';
+
+type WeatherCondition = "Clouds" | "Clear" | "Rain" | "Atmosphere" | "Snow" | "Drizzle" | "Thunderstorm";
+
+const icons: Record<WeatherCondition, keyof typeof Fontisto.glyphMap> = {
+  Clear: "day-sunny",
+  Clouds: "cloudy",
+  Rain: "rain",
+  Atmosphere: "cloudy-gusts",
+  Snow: "snow",
+  Drizzle: "day-rain",
+  Thunderstorm: "lightning",
+};
+
+function isWeatherCondition(condition: string): condition is WeatherCondition {
+  return condition in icons;
+}
+
+interface WeatherData {
+  dt_txt: string;
+  main: {
+    temp: number;
+  };
+  weather: { main: string, description: string }[];
+}
 
 export default function HomeScreen() {
+  const [city, setCity] = useState<string | null>("Loading");
+  const [days, setDays] = useState<WeatherData[]>([]);
+  const [ok, setOk] = useState(true);
+
+  const ask = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+    const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync({ latitude, longitude }, { useGoogleMaps: false });
+
+    setCity(location[0].city);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`);
+    const { list }: { list: WeatherData[] } = await response.json();
+    const filteredList = list.filter(({ dt_txt }) => dt_txt.endsWith("00:00:00"));
+    setDays(filteredList);
+  };
+
+  useEffect(() => {
+    ask();
+  }, []);
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles.container}>
+      <StatusBar style='light' />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>{city}</Text>
+      </View>
+
+      <ScrollView
+        pagingEnabled
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
+        {days.length === 0 ? (
+          <View style={styles.day}>
+            <ActivityIndicator color="white" size='large' style={{ marginTop: 10 }} />
+          </View>
+        ) : (
+          days.map((day, index) => (
+            <View key={index} style={styles.day}>
+              <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: "space-between" }}>
+                <Text style={styles.temp}>{Math.round(day.main.temp - 273.15)}°C</Text>
+                <Fontisto
+                  name={isWeatherCondition(day.weather[0].main) ? icons[day.weather[0].main] : "cloudy"}
+                  size={60}
+                  color="white"
+                />
+              </View>
+              <Text style={styles.description}>{day.weather[0].main}</Text>
+              <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+            </View>
+          ))
+        )}
+      </ScrollView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  container: {
+    flex: 2,
+    backgroundColor: 'pink',
+  },
+  city: {
+    flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
-    gap: 8,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  cityName: {
+    color: 'white',
+    fontSize: 68,
+    fontWeight: "500",
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  weather: {},
+  day: {
+    width: SCREEN_WIDTH,
+    flex: 1,
+    alignItems: 'center',
   },
+  temp: {
+    color: 'white',
+    marginTop: 50,
+    fontSize: 100,
+  },
+  description: {
+    color: 'white',
+    marginTop: -30,
+    fontSize: 60,
+  },
+  tinyText: {
+    color: 'white',
+    fontSize: 20,
+  }
 });
